aa998c8254e1b34419fa701cce2747f9fb21bb2d
Martin Eberl <martin.eberl@detego.com>
2024-01-01 17:31:34 +0100

    Update pod structure according to new one

---
+++ Example/Pods/Target Support Files/EMStore-iOS/EMStore-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_EMStore_iOS : NSObject
+@end
+@implementation PodsDummy_EMStore_iOS
+@end
---
+++ Example/Pods/Target Support Files/EMStore-tvOS/EMStore-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_EMStore_tvOS : NSObject
+@end
+@implementation PodsDummy_EMStore_tvOS
+@end
